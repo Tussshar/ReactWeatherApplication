0db9f47f5fc79b58164ff0f7ba50ca8978567218
Tussshar <tusharkale52@gmail.com>
2017-07-29 18:46:43 -0400

    Changes in the Route path

--- app/app.jsx
+++ app/app.jsx
@@ -21,10 +21,10 @@ var Examples = require('Examples');
 
 ReactDOM.render(
   <Router history={hashHistory}>
-    <Route path="//" component={Main}>
+    <Route path="/" component={Main}>
       //This is our first route. Using path we make sure what we want to show as a route of our application
-      <Route path="/about" component={About}/>
-      <Route path="/examples" component={Examples}/>
+      <Route path="about" component={About}/>
+      <Route path="examples" component={Examples}/>
       <IndexRoute component={Weather}/>
       //IndexRoute only take one argument
       //For Route we use Link and for IndexRoute we use IndexLink
