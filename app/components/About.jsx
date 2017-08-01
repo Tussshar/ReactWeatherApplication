6ef66bf44b23014b51635d4fd0aa4375263f8a35
Tussshar <tusharkale52@gmail.com>
2017-07-31 23:19:56 -0400

    Adding styles to About second commit

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -21,17 +21,25 @@ var About = (props) => {
         <p>This is a weather application build on React.
           I have built this for 'The Complete React Web App Developer Course'.
         </p>
-        <p>
-          Here are some of the tools I used:
-        </p>
-        <li>
-          <a href="https://facebook.github.io/react">React</a> - This was the
-            JavaScript framework used.
-        </li>
-        <li>
-          <a href="http://openweathermap.org">Open Weather Map</a> - I used
-            Open Weather Map to search for weather data by city name.
-        </li>
+        <div>
+          <p>
+            Here are some of the tools I used:
+          </p>
+          <li>
+            <a href="https://facebook.github.io/react">React</a> - This was the
+              JavaScript framework used.
+          </li>
+          <li>
+            <a href="http://openweathermap.org">Open Weather Map</a> - I used
+              Open Weather Map to search for weather data by city name.
+          </li>
+        </div>
+        <div>
+          <p>You can find the github repo for this project at the following link:</p>
+          <li>
+            <a href="https://github.com/Tussshar/ReactWeatherApplication">React Weather Application</a>
+          </li>
+        </div>
     </div>
   )
 };
