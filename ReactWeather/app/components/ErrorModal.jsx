33dc98b71ab892fff2f3f3b35b7736c8c5356b60
Tussshar <tusharkale52@gmail.com>
2017-08-05 08:54:24 -0400

    Adding Modal File

--- ReactWeather/app/components/ErrorModal.jsx
+++ ReactWeather/app/components/ErrorModal.jsx
@@ -9,6 +9,7 @@ var ErrorModal = React.createClass({
 
     componentDidMount: gets rendered after the elements have been rendered in the DOM
   */
+  /*Adding random comment*/
   getDefaultProps: function(){
     return {
       title: 'Error'
